c1a3224484c365546877d2443bb0d0faf05151ba
cwfink <cwfink@berkeley.edu>
2018-09-06 11:34:02 -0700

    added diagnostic plotting functionality to OFnonlin class

--- pycdms/detcal/misc/utils.py
+++ pycdms/detcal/misc/utils.py
@@ -4,7 +4,8 @@ from scipy.ndimage.interpolation import shift
 from scipy.optimize import least_squares
 import numpy as np
 from numpy.fft import rfft, fft, ifft, fftfreq, rfftfreq
-    
+import matplotlib.pyplot as plt
+   
 
 def calc_psd(x, fs=1.0, folded_over=True):
     """Return the PSD of an n-dimensional array, assuming that we want the PSD of the last axis.
@@ -481,12 +482,7 @@ class OFnonlin(object):
                 Array of amplitude values as a function of freuqncy
         """
         tau_r = self.taurise
-        omega = 2*np.pi*self.freqs
-        delta = tau_r-tau_f
-        rat = tau_r/tau_f
-        amp = A/(rat**(-tau_r/delta)-rat**(-tau_f/delta))
-        pulse = amp*np.abs(tau_r-tau_f)/(1+omega*tau_f*1j)*1/(1+omega*tau_r*1j)*np.exp(-omega*t0*1.0j)
-        return pulse*np.sqrt(self.df)
+        return self.twopole(A, tau_r,tau_f,t0)
     
     def residuals(self, params):
         """
@@ -530,7 +526,7 @@ class OFnonlin(object):
         return sum(np.abs(self.data-model)**2/self.error**2)/(len(self.data)-self.dof)
 
     def fit_falltimes(self,pulse, lgcdouble = False, errscale = 1, guess = None, taurise = None, 
-                      lgcfullrtn = False):
+                      lgcfullrtn = False, lgcplot = False):
         """
         Function to do the fit
         
@@ -551,6 +547,8 @@ class OFnonlin(object):
             lgcfullrtn: bool, optional
                 If False, only the best fit parameters are returned. If True, the errors in the fit parameters,
                 the covariance matrix, and chi squared statistic are returned as well.
+            lgcplot: bool, optional
+                If True, diagnostic plots are returned. 
         Returns
         -------
             variables: tuple
@@ -599,10 +597,7 @@ class OFnonlin(object):
             taufallguess = (tauind-maxind)/self.fs
             tauriseguess = 20e-6
             t0guess = maxind/self.fs
-            if lgcdouble:
-                p0 = (ampguess, tauriseguess, taufallguess, t0guess)
-            else:
-                p0 = (ampguess, taufallguess, t0guess)
+
         else:
             maxind = np.argmax(pulse)
             ampguess = np.mean(pulse[maxind-7:maxind+7])
@@ -637,7 +632,106 @@ class OFnonlin(object):
         jac = result['jac']
         cov = np.linalg.inv(np.dot(np.transpose(jac),jac))
         errors = np.sqrt(cov.diagonal())
+        
+        if lgcplot:
+            plotnonlin(self,pulse, variables)
+        
         if lgcfullrtn:
             return variables, errors, cov, chi2
         else:
             return variables
+
+        
+        
+        
+        
+        
+def plotnonlin(OFnonlinOBJ,pulse, params):
+    """
+    Diagnostic plotting of non-linear pulse fitting
+    
+    Parameters
+    ----------
+        OFnonlinOBJ: OFnonlin object
+            The OFnonlin fit object to be plotted
+        pulse: ndarray
+            The raw trace to be fit
+        params: tuple
+            Tuple containing best fit paramters
+            
+    Returns
+    -------
+        None
+    """
+    if OFnonlinOBJ.lgcdouble:
+        A,tau_r,tau_f,t0 = params
+    else:
+        A,tau_f,t0 = params
+        tau_r = OFnonlinOBJ.taurise
+    variables = [A,tau_r,tau_f,t0]
+    ## get indices to define window ##
+    t0ind = int(t0*OFnonlinOBJ.fs) #location of timeoffset
+    nmin = t0ind - int(5*tau_r*OFnonlinOBJ.fs) # 5 falltimes before offset
+    nmax = t0ind + int(7*tau_f*OFnonlinOBJ.fs) # 7 falltimes after offset
+    
+    
+    f = OFnonlinOBJ.freqs
+    cf = f > 0
+    f = f[cf]
+    error = OFnonlinOBJ.error[cf]
+    
+    fig, axes = plt.subplots(2,2,figsize = (12,8))
+    fig.suptitle('Non-Linear Two Pole Fit', fontsize = 18)
+    
+    axes[0][0].grid(True, linestyle = 'dashed')
+    axes[0][0].set_title(f'Frequency Domain Trace')
+    axes[0][0].set_xlabel(f'Frequency [Hz]')
+    axes[0][0].set_ylabel('Amplitude [A/$\sqrt{\mathrm{Hz}}$]')
+    axes[0][0].loglog(f, np.abs(OFnonlinOBJ.data[cf]),c = 'g', label = 'Pulse', alpha = .75)
+    axes[0][0].loglog(f, np.abs(OFnonlinOBJ.twopole(*variables))[cf], c = 'r', label = 'Fit') 
+    axes[0][0].loglog(f, error,c = 'b', label = '$\sqrt{PSD}$', alpha = .75)
+    axes[0][0].tick_params(which = 'both', direction='in', right = True, top = True)
+    
+    axes[0][1].grid(True, linestyle = 'dashed')
+    axes[0][1].set_title(f'Time Series Trace (Zoomed)')
+    axes[0][1].set_xlabel(f'Time [ms]')
+    axes[0][1].set_ylabel(f'Amplitude [Amps]')
+    axes[0][1].plot(OFnonlinOBJ.time[nmin:nmax]*1e3, pulse[nmin:nmax], c = 'g', label = 'Pulse', alpha = 0.75)
+    axes[0][1].plot(OFnonlinOBJ.time[nmin:nmax]*1e3, OFnonlinOBJ.twopoletime(*variables)[nmin:nmax], c = 'r', label = 'time domain')
+    axes[0][1].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axes[0][1].tick_params(which = 'both', direction='in', right = True, top = True)
+
+    
+    axes[1][0].grid(True, linestyle = 'dashed')
+    axes[1][0].set_title(f'Time Series Trace (Full)')
+    axes[1][0].set_xlabel(f'Time [ms]')
+    axes[1][0].set_ylabel(f'Amplitude [Amps]')
+    axes[1][0].plot(OFnonlinOBJ.time*1e3, pulse, c = 'g', label = 'Pulse', alpha = 0.75)
+    axes[1][0].plot(OFnonlinOBJ.time*1e3, OFnonlinOBJ.twopoletime(*variables), c = 'r', label = 'time domain')
+    axes[1][0].ticklabel_format(style='sci', axis='y', scilimits=(0,0))
+    axes[1][0].tick_params(which = 'both', direction='in', right = True, top = True)
+
+    
+    axes[1][1].plot([],[],  c = 'r', label = 'Best Fit')
+    axes[1][1].plot([],[],  c = 'g', label = 'Raw Data')
+    axes[1][1].plot([],[],  c = 'b', label = '$\sqrt{PSD}$')
+    
+    for ii in range(len(params)):
+        axes[1][1].plot([],[],  linestyle = ' ')
+   
+    labels = [f'Amplitude: {A*1e6:.4f} [$\mu$A]'\
+    ,f'τ$_f$: {tau_f*1e6:.4f} [$\mu$s]'\
+     ,f'$t_0$: {t0*1e3:.4f} [ms]'\
+    ,f'τ$_r$: {tau_r*1e6:.4f} [$\mu$s]']
+    
+    lines = axes[1][1].get_lines()
+    legend1 = plt.legend([lines[i] for i in range(3, 3+len(params))], [labels[ii] for ii  in range(len(params))]
+    , loc=1)
+    legend2 = plt.legend([lines[i] for i in range(0,3)], ['Best Fit', 'Raw Data', '$\sqrt{PSD}$'], loc = 2)
+
+    axes[1][1].add_artist(legend1)
+    axes[1][1].add_artist(legend2)
+    axes[1][1].axis('off')
+   
+    plt.tight_layout()
+    plt.subplots_adjust(top=0.9)
