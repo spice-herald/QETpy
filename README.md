505cd414eed1e5f50275968d024fa10b1291437c
Samuel Watkins <samwatkins@galba.stanford.edu>
2018-09-07 11:36:15 -0700

    update setup with a clean command

--- README.md
+++ README.md
@@ -1,7 +1,8 @@
 # pycdms Package
 
-To install pycdms, from the top-level directory of the repo, type the following line into your command line
+To install pycdms, from the top-level directory of the repo, type the following lines into your command line
 
+`python setup.py clean`
 `python setup.py install --user`
     
 If you'd like to test the package, the `demos/` directory contains tutorials for various ways it can be used.
--- setup.py
+++ setup.py
@@ -1,5 +1,44 @@
-from setuptools import setup, find_packages
+import os
+import glob
+import shutil
+from setuptools import setup, find_packages, Command
 
-setup(name="pycdms", version="0.2.0", description="SuperCDMS Python Tools", 
-      author="Samuel Watkins, Caleb Fink", author_email="samwatkins@berkeley.edu, cwfink@berkeley.edu", 
-      url="https://github.com/cwfink/python_analysis_dev_tools", packages=find_packages(), zip_safe=False)
+
+class CleanCommand(Command):
+    """Custom clean command to tidy up the project root."""
+    CLEAN_FILES = './build ./dist ./*.pyc ./*.tgz ./*.egg-info'.split(' ')
+
+    user_options = []
+
+    def initialize_options(self):
+        pass
+
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        here = os.path.dirname(os.path.abspath(__file__))
+
+        for path_spec in self.CLEAN_FILES:
+            # Make paths absolute and relative to this path
+            abs_paths = glob.glob(os.path.normpath(os.path.join(here, path_spec)))
+            for path in [str(p) for p in abs_paths]:
+                if not path.startswith(here):
+                    # Die if path in CLEAN_FILES is absolute + outside this directory
+                    raise ValueError("%s is not a path inside %s" % (path, here))
+                print('removing %s' % os.path.relpath(path))
+                shutil.rmtree(path)
+
+setup(
+    name="pycdms", 
+    version="0.2.0", 
+    description="SuperCDMS Python Tools", 
+    author="Samuel Watkins, Caleb Fink", 
+    author_email="samwatkins@berkeley.edu, cwfink@berkeley.edu", 
+    url="https://github.com/cwfink/python_analysis_dev_tools", 
+    packages=find_packages(), 
+    zip_safe=False,
+    cmdclass={
+            'clean': CleanCommand,
+            }
+    )
