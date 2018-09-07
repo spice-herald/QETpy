320e49902738140cd7e7cf66643157a27def2d86
cwfink <cwfink@berkeley.edu>
2018-09-07 11:18:13 -0700

    changed version from 0.1 > 0.2.0

--- setup.py
+++ setup.py
@@ -1,5 +1,5 @@
 from setuptools import setup, find_packages
 
-setup(name="pycdms", version="0.1", description="SuperCDMS Python Tools", 
+setup(name="pycdms", version="0.2.0", description="SuperCDMS Python Tools", 
       author="Samuel Watkins, Caleb Fink", author_email="samwatkins@berkeley.edu, cwfink@berkeley.edu", 
       url="https://github.com/cwfink/python_analysis_dev_tools", packages=find_packages(), zip_safe=False)
