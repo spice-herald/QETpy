659a577c4d7650eb02f994340bd67efcd0ddec5e
Samuel Watkins <samwatkins@galba.stanford.edu>
2018-09-10 10:37:19 -0700

    add more test functions

---
+++ test/test_fitting.py
@@ -0,0 +1,103 @@
+import numpy as np
+from pycdms.fitting import ofamp, chi2lowfreq, calc_offset, OFnonlin, MuonTailFit
+
+
+def test_ofamp():
+    fs = 625e3
+    tracelength = 32000
+    psd = np.ones(tracelength)
+
+    # Dummy pulse template
+    nbin = len(psd)
+    ind_trigger = round(nbin/2)
+    tt = 1.0/fs *(np.arange(1, nbin+1)-ind_trigger)
+    lgc_b0 = tt < 0.0
+
+    # pulse shape
+    tau_rise = 20.0e-6
+    tau_fall = 80.0e-6
+    template = np.exp(-tt/tau_fall)-np.exp(-tt/tau_rise)
+    template[lgc_b0] = 0.0
+    template = template/max(template)
+    signal = template + np.random.randn(len(template))/10
+    signal = np.roll(signal,20)
+    res = ofamp(signal, testtemplate, psd, fs, lgcsigma=True, nconstrain=100, withdelay=True)
+    
+    assert len(res)>0
+    
+def test_chi2lowfreq():
+    fs = 625e3
+    tracelength = 32000
+    psd = np.ones(tracelength)
+
+    # Dummy pulse template
+    nbin = len(psd)
+    ind_trigger = round(nbin/2)
+    tt = 1.0/fs *(np.arange(1, nbin+1)-ind_trigger)
+    lgc_b0 = tt < 0.0
+
+    # pulse shape
+    tau_rise = 20.0e-6
+    tau_fall = 80.0e-6
+    template = np.exp(-tt/tau_fall)-np.exp(-tt/tau_rise)
+    template[lgc_b0] = 0.0
+    template = template/max(template)
+    signal = template + np.random.randn(len(template))/10
+    signal = np.roll(signal,20)
+    res = ofamp(signal, testtemplate, psd, fs, lgcsigma=True, nconstrain=100)
+
+    chi2low = chi2lowfreq(signal, template, res[0], res[1], psd, fs)
+    
+    assert chi2low>0
+    
+def test_OFnonlin():
+
+    fs = 625e3
+    tracelength = 32000
+    psd = np.ones(tracelength)
+
+    # Dummy pulse template
+    nbin = len(psd)
+    ind_trigger = round(nbin/2)
+    tt = 1.0/fs *(np.arange(1, nbin+1)-ind_trigger)
+    lgc_b0 = tt < 0.0
+
+    # pulse shape
+    tau_rise = 20.0e-6
+    tau_fall = 80.0e-6
+    template = np.exp(-tt/tau_fall)-np.exp(-tt/tau_rise)
+    template[lgc_b0] = 0.0
+    template = template/max(template)
+    signal = template + np.random.randn(len(template))/10
+    signal = np.roll(signal, 20)
+
+    nlin = OFnonlin(psd, fs, template=template)
+    res1 = nlin.fit_falltimes(signal, lgcdouble=True, lgcfullrtn=True, lgcplot=True)
+    res2 = nlin.fit_falltimes(signal, lgcdouble=False, lgcfullrtn=True, lgcplot=True, taurise=20e-6)
+    
+    assert len(res1)>0
+    assert len(res2)>0
+    
+def test_MuonTailFit():
+    
+    fs = 625e3
+    tracelength = 32000
+    psd = np.ones(tracelength)
+
+    # Dummy pulse template
+    nbin = len(psd)
+    ind_trigger = 0
+    tt = 1.0/fs *(np.arange(1, nbin+1)-ind_trigger)
+    lgc_b0 = tt < 0.0
+
+    # pulse shape
+    tau_fall = 20e-3
+    template = np.exp(-tt/tau_fall)
+    template[lgc_b0] = 0.0
+    template = template/max(template)
+    signal = template + np.random.randn(len(template))/10
+
+    mtail = MuonTailFit(psd, fs)
+    res = mtail.fitmuontail(signal, lgcfullrtn=True)
+    
+    assert len(res)>0
---
+++ test/test_utils.py
@@ -0,0 +1,55 @@
+import numpy as np
+from pycdms.utils import (align_traces, calc_offset, calc_psd, fill_negatives, 
+                          iterstat, lowpassfilter, removeoutliers, stdcomplex)
+
+def test_align_traces():
+    traces = np.random.randn(100, 32000)
+    res = align_traces(traces)
+    
+    assert len(res)>0
+
+def test_calc_offset():
+    traces = np.random.randn(100, 32000)
+    res = calc_offset(traces, is_didv=True)
+    
+    assert len(res)>0
+    
+def test_calc_psd():
+    traces = np.random.randn(100, 32000)
+    res = calc_psd(traces)
+
+    assert len(res)>0
+    
+def test_iterstat():
+    traces = np.random.randn(100, 32000)
+    offsets = traces.mean(axis=1)
+    res = iterstat(offsets)
+    
+    assert len(res)>0
+    
+def test_removeoutliers():
+    traces = np.random.randn(100, 32000)
+    offsets = traces.mean(axis=1)
+    res = removeoutliers(offsets)
+    
+    assert len(res)>0
+    
+def test_stdcomplex():
+    vals = np.array([3.0+3.0j, 0.0, 0.0])
+    res = stdcomplex(vals)
+    
+    assert res == np.sqrt(2)*(1.0+1.0j)
+
+def test_lowpassfilter():
+    traces = np.random.randn(100)
+    res = lowpassfilter(traces)
+    
+    assert res.shape == traces.shape
+
+def test_fill_negatives():
+    traces = np.random.randn(100, 32000)
+    res = fill_negatives(traces)
+    
+    assert res.shape == traces.shape
+    
+    
