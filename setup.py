3f1b486242c19e741b4aac823580276512a372c8
caleb fink <36051548+cwfink@users.noreply.github.com>
2018-12-10 22:16:20 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -31,7 +31,7 @@ class CleanCommand(Command):
 
 setup(
     name="QETpy", 
-    version="0.4.0.dev0", 
+    version="1.0.0.dev0", 
     description="TES Detector Calibration and Analysis Python Tools", 
     author="Samuel Watkins, Caleb Fink", 
     author_email="samwatkins@berkeley.edu, cwfink@berkeley.edu", 
