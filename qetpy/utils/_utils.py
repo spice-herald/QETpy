a6e4fdca085aa7cd6af369183819e4a5fc697fcc
Samuel Watkins <samwatkins@galba.stanford.edu>
2019-03-13 14:26:16 -0700

    removing make_template and shift (will re-add via PR)

--- qetpy/utils/_utils.py
+++ qetpy/utils/_utils.py
@@ -5,81 +5,8 @@ from scipy.ndimage.interpolation import shift
 
 __all__ = ["make_decreasing", "calc_offset", "lowpassfilter", "align_traces", "get_offset_from_muon",
            "powertrace_simple", "energy_absorbed", "stdcomplex", "slope",
-           "fill_negatives", "shift", "make_template"]
+           "fill_negatives"]
 
-
-def shift(arr, num, fill_value=0):
-    """
-    Function for shifting the values in an array by a certain number of indices, filling
-    the values of the bins at the head or tail of the array with fill_value.
-    
-    Parameters
-    ----------
-    arr : array_like
-        Array to shift values in.
-    num : float
-        The number of values to shift by. If positive, values shift to the right. If negative, 
-        values shift to the left.
-        If num is a non-whole number of bins, arr is linearly interpolated
-    fill_value : scalar, optional
-        The value to fill the bins at the head or tail of the array with.
-    
-    Returns
-    -------
-    result : ndarray
-        The resulting array that has been shifted and filled in.
-    
-    """
-    
-    result = np.empty_like(arr)
-    
-    if float(num).is_integer():
-        num = int(num) # force num to int type for slicing
-        
-        if num > 0:
-            result[:num] = fill_value
-            result[num:] = arr[:-num]
-        elif num < 0:
-            result[num:] = fill_value
-            result[:num] = arr[-num:]
-        else:
-            result[:] = arr
-    else:
-        result = ndimage.shift(arr, num, order=1, mode='constant', cval=fill_value)
-        
-    return result
-
-
-def make_template(t, tau_r, tau_f, offset=0):
-    """
-    Function to make an ideal pulse template in time domain with single pole exponential rise
-    and fall times, and a given time offset. The template will be returned with the maximum
-    pulse height normalized to one. The pulse, by default, begins at the center of the trace, 
-    which can be left or right shifted via the `offset` optional argument.
-    
-    Parameters
-    ----------
-    t : ndarray
-        Array of time values to make the pulse with
-    tau_r : float
-        The time constant for the exponential rise of the pulse
-    tau_f : float
-        The time constant for the exponential fall of the pulse
-    offset : int
-        The number of bins the pulse template should be shifted
-        
-    Returns
-    -------
-    template_normed : array
-        the pulse template in time domain
-        
-    """
-    
-    pulse = np.exp(-t/tau_f)-np.exp(-t/tau_r)
-    pulse_shifted = shift(pulse, len(t)//2 + offset)
-    template_normed = pulse_shifted/pulse_shifted.max()
-    
-    return template_normed
 
 def make_decreasing(y, x=None):
     """
