49c8d41fd4ed9e0e77e51f457d18a444016632e3
Samuel Watkins <samwatkins@galba.stanford.edu>
2019-09-17 08:12:09 -0700

    update dIdV test to do all plot options

--- test/test_detcal.py
+++ test/test_detcal.py
@@ -14,7 +14,7 @@ def test_autocuts():
     fs = 625e3
     cut = autocuts(traces, fs=fs)
     assert len(cut)>0
-    
+
 def test_didv():
     # Setting various parameters that are specific to the dataset
     Rshunt = 5.0e-3
@@ -34,15 +34,11 @@ def test_didv():
 
     convToAmps = Rfb * loopgain * drivergain * ADCperVolt
 
-    saveResults = False
-    
     pathtodata = os.path.join(THIS_DIR, 'data/example_traces.h5')
 
     # load the dataset
     with h5py.File(pathtodata,'r') as f:
         rawTraces = np.array(f["rawTraces"])
-
-    fileSaveName = "example_traces_data"
 
     # set the priors information, for use the priors fitting
     priors = np.zeros(7)
@@ -53,20 +49,34 @@ def test_didv():
     invpriorsCov[1,1] = 1.0/dR0**2
     dt0=-18.8e-6
     
-    didvfit = DIDV(rawTraces, fs, sgFreq, sgAmp, Rshunt, tracegain=convToAmps, priors=priors, 
-                   invpriorscov=invpriorsCov)
+    didvfit = DIDV(
+        rawTraces,
+        fs,
+        sgFreq,
+        sgAmp,
+        Rshunt,
+        tracegain=convToAmps,
+        priors=priors,
+        invpriorscov=invpriorsCov,
+    )
+
     didvfit.processtraces()
     didvfit.doallfits()
-    didvfit.plot_full_trace()
-    didvfit.plot_full_trace(poles="all",plotpriors=True)
-    didvfit.plot_single_period_of_trace(poles=[2,3], lgcsave=True, savename="test")
-    didvfit.plot_zoomed_in_trace(poles=2)
-    didvfit.plot_didv_flipped()
-    didvfit.plot_re_im_didv(poles=[2,3])
-    didvfit.plot_abs_phase_didv()
+    didvfit.plot_full_trace(lgcsave=True, savename="test")
+    didvfit.plot_full_trace(poles="all", plotpriors=True)
+    didvfit.plot_single_period_of_trace(poles=[2, 3], lgcsave=True, savename="test")
+    didvfit.plot_single_period_of_trace(poles="all", plotpriors=True)
+    didvfit.plot_zoomed_in_trace(poles=2, plotpriors=True)
+    didvfit.plot_zoomed_in_trace(poles="all", lgcsave=True, savename="test")
+    didvfit.plot_didv_flipped(plotpriors=True)
+    didvfit.plot_didv_flipped(poles=[2, 3], lgcsave=True, savename="test")
+    didvfit.plot_re_im_didv(poles=[2, 3], lgcsave=True, savename="test")
+    didvfit.plot_re_im_didv(poles="all", plotpriors=True)
+    didvfit.plot_abs_phase_didv(poles=[2, 3], lgcsave=True, savename="test")
+    didvfit.plot_abs_phase_didv(poles="all", plotpriors=True)
 
     assert len(didvfit.fitparams2) > 0
-    
+
 def test_iv():
     pathtodata = os.path.join(THIS_DIR, "data/test_iv_data.npz")
     testdata = np.load(pathtodata)
@@ -76,66 +86,135 @@ def test_iv():
     vb_err = testdata["vb_err"]
     rload = testdata["rload"]
     rload_err = testdata["rload_err"]
-    
+
     ivdata = IV(dites, dites_err, vb, vb_err, rload, rload_err, ["A","B","C"])
     ivdata.calc_iv()
-    
+
     ivdata.plot_all_curves()
-    
+
     assert len(ivdata.r0) > 0
-    
+
 def test_noise():
     pathtodata = os.path.join(THIS_DIR, "data/traces.npy")
     traces_PT_on = np.load(pathtodata)
-    
+
     savePath = '' #user needs to define new path
 
     sampleRate = 625e3 #define sample rate
-    channels = [ 'PCS1' , 'PES1' , 'PFS1' , 'PAS2' , 'PBS2' , 'PES2' , 'PDS2' ] #define the channel names
+    channels = ['PCS1', 'PES1', 'PFS1', 'PAS2', 'PBS2', 'PES2', 'PDS2'] #define the channel names
     g124_noise = Noise(traces_PT_on, sampleRate, channels) #initialize a noise object
     g124_noise.name = 'G124 SLAC Run 37 Pulse Tube On'
-    
+
     g124_noise.calculate_psd()
     g124_noise.calculate_corrcoeff()
     g124_noise.calculate_csd()
-    
+
     g124_noise.calculate_uncorr_noise()
     g124_noise.plot_psd(lgcoverlay=True)
     g124_noise.plot_corrcoeff(lgcsave=False, lgcsmooth=True, nwindow=13)
     g124_noise.plot_csd(whichcsd=['66','26'])
     g124_noise.plot_reim_psd()
     g124_noise.plot_decorrelatednoise(lgccorrelated=True)
-    
-    noise_sim = TESnoise(freqs = g124_noise.freqs[1:])
-    #plot_noise_sim(g124_noise.freqs, g124_noise.psd[0,:], noise_sim, istype='power')
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='power', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=None, ylims=None)
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='current', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=None, ylims=None)
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='sc', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=None, ylims=None)
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='normal', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=None, ylims=None)
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='power', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=(10,1e5), ylims=(1e-12, 1e-9))
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='current', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=(10,1e5), ylims=(1e-12, 1e-9))
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='sc', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=(10,1e5), ylims=(1e-12, 1e-9))
-    plot_noise_sim(f=g124_noise.freqs, psd=g124_noise.psd[0,:], noise_sim=noise_sim,
-                   istype='normal', qetbias=1, lgcsave=False, figsavepath='', 
-                  xlims=(10,1e5), ylims=(1e-12, 1e-9))
 
-    
+    noise_sim = TESnoise(freqs=g124_noise.freqs[1:])
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='power',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=None,
+        ylims=None,
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='current',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=None,
+        ylims=None,
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='sc',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=None,
+        ylims=None,
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='normal',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=None,
+        ylims=None,
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='power',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=(10, 1e5),
+        ylims=(1e-12, 1e-9),
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='current',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=(10, 1e5),
+        ylims=(1e-12, 1e-9),
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0, :],
+        noise_sim=noise_sim,
+        istype='sc',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=(10, 1e5),
+        ylims=(1e-12, 1e-9),
+    )
+
+    plot_noise_sim(
+        f=g124_noise.freqs,
+        psd=g124_noise.psd[0,:],
+        noise_sim=noise_sim,
+        istype='normal',
+        qetbias=1,
+        lgcsave=False,
+        figsavepath='',
+        xlims=(10, 1e5),
+        ylims=(1e-12, 1e-9),
+    )
+
+
     assert len(g124_noise.psd) > 0
     assert len(noise_sim.freqs) > 0
-    
-    
-    
+
