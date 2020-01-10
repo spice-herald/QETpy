e4d09204640af697545e793084a984d29b1fcfd2
caleb fink <cwfink@berkeley.edu>
2020-01-10 16:17:30 -0600

    Update __init__.py

--- qetpy/plotting/__init__.py
+++ qetpy/plotting/__init__.py
@@ -1,3 +1,7 @@
-from ._plotting import * 
+from ._didv_plotting import *
+from ._fitting_plotting import *
+from ._iv_plotting import *
+from ._noise_plotting import *
+from ._of_nsmb_plotting import *
 from ._ibis_plotting import *
 
