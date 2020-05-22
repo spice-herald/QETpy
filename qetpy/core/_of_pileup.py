4720e48f009d8c0fe4b95737bb7a2384c53ba5b6
Samuel Watkins <samwatkins@berkeley.edu>
2020-05-22 20:32:25 +0000

    add docstrings for PileupOF

--- qetpy/core/_of_pileup.py
+++ qetpy/core/_of_pileup.py
@@ -7,9 +7,50 @@ __all__ = [
 ]
 
 class PileupOF(object):
+    """
+    Class for efficient calculation of the Pileup Optimum Filter (OF).
+
+    Attributes
+    ----------
+    iter_res : ndarray
+        The amplitudes, time-shift values, and chi-square from the
+        iterative pileup optimum filter,
+        `qetpy.OptimumFilter.ofamp_pileup_iterative`.
+
+    """
 
     def __init__(self, signal, template, psd, fs, ac_coupled=True,
                  integralnorm=False):
+        """
+        Initalization of the PileupOF class.
+
+        Parameters
+        ----------
+        signal : ndarray
+            The signal that we want to apply the pileup optimum filter
+            to (units should be Amps).
+        template : ndarray
+            The pulse template to be used for the pileup optimum filter
+            (should be normalized to a max height of 1 beforehand).
+        psd : ndarray
+            The two-sided psd that will be used to describe the noise
+            in the signal (in Amps^2/Hz)
+        fs : ndarray
+            The sample rate of the data being taken (in Hz).
+        ac_coupled : bool, optional
+            If True, the zero frequency bin of the psd
+            will be ignored (i.e. set to infinity) when calculating
+            the optimum amplitude. If False, then the zero frequency
+            bin is kept. Default is True.
+        integralnorm : bool, optional
+            If set to True, then the template will be normalized to
+            have an integral of 1, and any optimum filters will instead
+            return the optimum integral in units of Coulombs. If set to
+            False, then the usual optimum filter amplitudes will be
+            returned (in units of Amps). Default is False.
+
+        """
+
         self._psd = np.zeros(len(psd))
         self._psd[:] = psd
         self._psd0 = psd[0]
@@ -51,7 +92,13 @@ class PileupOF(object):
 
         self._update_signal(signal)
 
+
     def _update_signal(self, signal):
+        """
+        Hidden function for recalculating parameters that depend on the
+        signal.
+
+        """
                 
         self._v = np.fft.fft(signal, axis=-1) / self._nbins / self._df
         self._qn = np.real(
@@ -81,16 +128,48 @@ class PileupOF(object):
             -2.0j * np.pi * self._exp_times[:, None] * self._freqs[None, :]
         )
 
+
     def update_signal(self, signal):
+        """
+        Function for updating all parameters that are signal-dependent,
+        so that the pileup optimum filter can be run on new data
+        without recalculating parameters unnecessarily.
+
+        Parameters
+        ----------
+        signal : ndarray
+            The signal that we want to apply the pileup optimum filter
+            to (units should be Amps).
+
+        Returns
+        -------
+        None
+
+        """
+
         self._OF.update_signal(signal)
         self._update_signal(signal)
 
+
     def _determine_tcutoff(self, cutoff=0.2):
-        tcutoff = np.argmin(np.abs(self._pmatrix_off - cutoff)) / self._fs
+        """
+        Hidden function to determine the cutoff on the time difference
+        between pulses where, after this point, the iterative pileup
+        optimum filter is 'good enough.'
+
+        """
+
         tcutoff = np.argmax(self._pmatrix_off < cutoff) / self._fs
+
         return tcutoff
 
+
     def _createpmatrices(self):
+        """
+        Hidden function to facilitate the precalculation of the
+        matrices used to determine the pileup amplitudes.
+
+        """
 
         npulses = 2
 
@@ -104,6 +183,12 @@ class PileupOF(object):
 
 
     def _get_amps(self, t0s):
+        """
+        Hidden function to calculate the amplitudes that correspond to
+        the inputted time offsets.
+
+        """
+
         p_ind = int(np.abs(t0s[0] - t0s[1]) * self._fs)
         pmatrix_inv = self._p_inv[p_ind]
         qvec = np.array(
@@ -114,6 +199,11 @@ class PileupOF(object):
 
 
     def _chi2(self, amps, t0s):
+        """
+        Hidden function to calculate the chi-square of the inputted
+        amplitude and time offsets.
+
+        """
 
         numer = self._v - self._s * sum(
             a * self._exp_array[int(t * self._fs)] for a, t in zip(amps, t0s)
@@ -121,7 +211,21 @@ class PileupOF(object):
 
         return np.real(np.dot(numer.conjugate() / self._psd, numer) * self._df)
 
+
     def run(self):
+        """
+        Runs the pileup optimum filter algorithm for 2 pulses.
+
+        Parameters
+        ----------
+        None
+
+        Returns
+        -------
+        res : ndarray
+            The results of the pileup optimum filter algorithm. The parameters returned are (amplitude 1, time offset 1, amplitude 2, time offset 2, chi-square). The fit from the pileup OF within the neighborhood of the first pulse is compared to the iterative pileup OF throughout the entire trace. The resulst of the pileup OF with the lowest chi-square are returned.
+
+        """
 
         npulses = 2
 
