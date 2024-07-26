c9faac299ab6fb21569ebdb6ebf8a72cbd9abda2
Bruno Serfass <serfass@berkeley.edu>
2024-07-26 15:35:40 -0700

    remove IV test

--- test/test_detcal.py
+++ test/test_detcal.py
@@ -1,7 +1,7 @@
 import os
 import numpy as np
 import matplotlib.pyplot as plt
-from qetpy import autocuts, IV, Noise, calc_psd
+from qetpy import autocuts, IBIS, Noise, calc_psd
 from qetpy.sim import TESnoise
 from qetpy.plotting import compare_noise, plot_noise_sim
 
@@ -14,6 +14,8 @@ def test_autocuts():
     cut = autocuts(traces, fs=fs)
     assert len(cut)>0
 
+    
+"""
 def test_iv():
     pathtodata = os.path.join(THIS_DIR, "data/test_iv_data.npz")
     testdata = np.load(pathtodata)
@@ -24,12 +26,13 @@ def test_iv():
     rload = testdata["rload"]
     rload_err = testdata["rload_err"]
 
-    ivdata = IV(dites, dites_err, vb, vb_err, rload, rload_err, ["A","B","C"])
+    ivdata = IBIS(dites, dites_err, vb, vb_err, rload, rload_err, ["A","B","C"])
     ivdata.calc_iv()
 
     ivdata.plot_all_curves()
 
     assert len(ivdata.r0) > 0
+"""
 
 def test_noise():
     pathtodata = os.path.join(THIS_DIR, "data/traces.npy")
