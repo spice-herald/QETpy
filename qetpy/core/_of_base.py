cleanup notebook demos, fix normalization

qetpy/core/_of_base.py
@@ -1078,6 +1078,11 @@ class OFBase:
         
         # convert to string if needed (if str, return same string)
         channel_name = convert_channel_list_to_name(channels)
+
+        # convert to 3D if needed
+        if csd.ndim == 1:
+            csd = csd[np.newaxis, np.newaxis, :]
+            
         
         # check if same length as template
         nbins = csd.shape[-1]
@@ -1302,10 +1307,10 @@ class OFBase:
             # add signal
             self._signals[chan] = signal
             
-            # add fft
+            # add fft ("forward" normalization)
             if calc_fft:
                 f, signal_fft = fft(signal, self._fs, axis=-1)
-                self._signals_fft[chan] = signal_fft/self._nbins/self._df
+                self._signals_fft[chan] = signal_fft/self._nbins
                 
     
     def calc_phi(self, channels, template_tag=None,
@@ -1372,14 +1377,7 @@ class OFBase:
             
             phi = np.einsum('kjn,kin->ijn', np.conjugate(template_fft), icovf)
 
-            #print(f'Phi shape: {phi.shape}')
-            #phi_loop = (
-            #    np.array([(template_fft[:,:,jnu].T).conjugate()
-            #              @ icovf[:,:,jnu] for jnu in range(self._nbins)
-            #    ], dtype='complex_')
-            #)
-            #phi = np.transpose(phi_loop, (2, 1, 0))
-
+        
             # save
             if channel_name not in self._phis:
                 self._phis[channel_name] = dict()
@@ -1455,25 +1453,6 @@ class OFBase:
             self._weights[channel_name][tag] =  weight
             self._iweights[channel_name][tag] = pinv(weight)
             
-            
-            """
-            # calculate weigth matrix
-            temp_w = np.zeros((ntmps, ntmps), dtype='complex_')
-            for itmp in range(ntmps):
-                for jtmp in range(ntmps):
-                    for jchan in range(nchans):
-                        temp_w[itmp,jtmp] += np.sum(
-                            phi[:,itmp,jchan]*template_fft[jchan,jtmp,:]
-                        )
-            # real
-            temp_w = np.real(temp_w)
-
-            # store
-            if channel_name not in self._iw_matrix:
-                self._iw_matrix[channel_name] = dict()
-            
-            self._iw_matrix[channel_name][matrix_tag] = pinv(temp_w)
-            """
 
             
     def calc_p_matrix(self, channels,

qetpy/core/_of_nxm.py
@@ -89,7 +89,7 @@ class OFnxm:
         self._channel_name = convert_channel_list_to_name(channels)
         self._channel_list = convert_channel_name_to_list(channels)
         self._nchans = len(self._channel_list)
-
+             
         # initialize number of templates (per channel)
         self._ntmps = None
         self._template_tag = None
@@ -270,19 +270,29 @@ class OFnxm:
         None
  
         """
-
+                
         # clear internal (signal) data
         self.clear()
-        
+
+                
         # update signal and do preliminary (signal) calculations
         # (like fft signal, filtered signal, signal matrix ...
         if signal is not None:
+
+            # check array
+            if not isinstance(signal, np.ndarray):
+                  raise ValueError('ERROR: signal should be a '
+                                   'numpy array!')
+
+            
+            # check dimension
+            if signal.ndim == 1:
+                signal = signal[np.newaxis, :]
             
             # check if correct size
-            if (not isinstance(signal, np.ndarray)
-                or signal.shape[0] != self._nchans):
+            if signal.shape[0] != self._nchans:
                 raise ValueError('ERROR: Wrong number of channels '
-                                'in "signal" array!')
+                                 'in "signal" array!')
             
             self._of_base.clear_signal()
 
@@ -436,16 +446,13 @@ class OFnxm:
             self._channel_name,
             template_tag=self._template_tag
         )
-        print(f'Shape signal filt: {signal_filt.shape}')
-        
+            
         # inverted weight matrix
         iw = self._of_base.iweights(self._channel_name,
                                     self._template_tag)
 
-        print(f'Shape iw: {iw.shape}')
         # calculate
         self._amps_alltimes = (iw @ signal_filt)
-        print(self._amps_alltimes.shape)
            
         # roll with pretrigger_samples 
         temp_amp_roll = np.zeros_like(self._amps_alltimes)
@@ -479,7 +486,6 @@ class OFnxm:
             template_tag=self._template_tag
         )
 
-        print(f'Shape signal filt td: {filt_signal_t.shape}')
         # calculate chi2
 
         #chi2base is a time independent scalar on the sum over all channels & freq bins